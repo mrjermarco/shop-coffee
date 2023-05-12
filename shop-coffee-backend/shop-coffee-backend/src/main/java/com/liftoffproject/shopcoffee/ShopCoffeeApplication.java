4f4cfc814aa43ffc0c192d69ff8a2d51c966c8db
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-05-11 19:26:12 -0500

    installed java dependencies for app

---
+++ shop-coffee-backend/shop-coffee-backend/src/main/java/com/liftoffproject/shopcoffee/ShopCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.liftoffproject.shopcoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCoffeeApplication.class, args);
+	}
+
+}
---
+++ shop-coffee-backend/shop-coffee-backend/src/test/java/com/liftoffproject/shopcoffee/ShopCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftoffproject.shopcoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
