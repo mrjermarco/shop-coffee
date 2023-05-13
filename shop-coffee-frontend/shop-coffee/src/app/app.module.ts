e177bfcd5005e7cd5231765c6d2b14f3d55e0337
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-05-13 12:03:30 -0500

    modified app.module to add import for the new admin-edit-page componnet

--- shop-coffee-frontend/shop-coffee/src/app/app.module.ts
+++ shop-coffee-frontend/shop-coffee/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminEditPageComponent } from './admin-edit-page/admin-edit-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminEditPageComponent
   ],
   imports: [
     BrowserModule,
