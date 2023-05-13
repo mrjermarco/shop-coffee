91c96e446a4dfebb5a925bb9a62d7ff0de1d8c85
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-05-13 12:00:10 -0500

    added admin-edit-page component

---
+++ shop-coffee-frontend/shop-coffee/src/app/admin-edit-page/admin-edit-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-edit-page',
+  templateUrl: './admin-edit-page.component.html',
+  styleUrls: ['./admin-edit-page.component.css']
+})
+export class AdminEditPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
