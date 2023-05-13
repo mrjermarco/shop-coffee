257dfdda04aa9e14147ffe6696d219d40617b7a8
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-05-13 12:43:04 -0500

    added routing for admin-edit-page

--- shop-coffee-frontend/shop-coffee/src/app/app-routing.module.ts
+++ shop-coffee-frontend/shop-coffee/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminEditPageComponent } from './admin-edit-page/admin-edit-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin-edit', component: AdminEditPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- shop-coffee-frontend/shop-coffee/src/app/app.module.ts
+++ shop-coffee-frontend/shop-coffee/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminEditPageComponent } from './admin-edit-page/admin-edit-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AdminEditPageComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
