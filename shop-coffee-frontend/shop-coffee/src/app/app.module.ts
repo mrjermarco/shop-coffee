ef6543d68a27b4f50f7b44b56f5d4c9bf4dde265
mrjermarco <107326902+mrjermarco@users.noreply.github.com>
2023-05-14 14:44:58 -0500

    added html/css for login amd register page

--- shop-coffee-frontend/shop-coffee/src/app/app.module.ts
+++ shop-coffee-frontend/shop-coffee/src/app/app.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { RegisterUserComponent } from './register-user/register-user.component';
+
+const routes: Routes =[
+  {path: 'user-login', component: UserLoginComponent},
+  {path: 'register-user', component: RegisterUserComponent}
+]
 
 @NgModule({
   declarations: [
@@ -14,7 +20,8 @@ import { RegisterUserComponent } from './register-user/register-user.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
