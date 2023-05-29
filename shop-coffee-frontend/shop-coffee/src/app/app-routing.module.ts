70531d4f8b50a1c74f8e1fc15e591bcc60947eeb
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-05-29 18:32:43 -0500

    added create-coffe and order-complete components

--- shop-coffee-frontend/shop-coffee/src/app/app-routing.module.ts
+++ shop-coffee-frontend/shop-coffee/src/app/app-routing.module.ts
@@ -5,13 +5,19 @@ import { UserLoginPageComponent } from './user-login-page/user-login-page.compon
 import { CreateCoffeeComponent } from './create-coffee/create-coffee.component';
 import { UserHomepageComponent } from './pages/user-homepage/user-homepage.component';
 import { OrderItemSpecificsComponent } from './pages/order-item-specifics/order-item-specifics.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import { OrderCompleteComponent } from './order-complete/order-complete.component';
 
 const routes: Routes = [
   { path: 'admin-edit', component: AdminEditPageComponent },
   { path: 'user-login', component: UserLoginPageComponent},
   { path: 'create-coffee', component: CreateCoffeeComponent},
   { path: 'user-homepage', component: UserHomepageComponent},
-  { path: 'order-item-specifics', component: OrderItemSpecificsComponent}
+  { path: 'order-item-specifics', component: OrderItemSpecificsComponent},
+  { path: 'home-page', component: HomePageComponent},
+  { path: 'checkout', component: CheckoutPageComponent},
+  { path: 'order-complete', component: OrderCompleteComponent}
 
 ];
 
